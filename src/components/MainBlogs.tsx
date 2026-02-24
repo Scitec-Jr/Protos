45117faea2fc138dba9f4de550b89d4b42275989
Vinícius Gonçalves <vinicius.devlp@gmail.com>
2026-02-24 02:23:20 -0300

    cache correction

--- src/components/MainBlogs.tsx
+++ src/components/MainBlogs.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+export const revalidate = 0;
+
 import Image from "next/image";
 import { db } from "@/lib/db";
 import { Blog } from "@/types/blog";
